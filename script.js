789ace9fd07edadadb17213c709f36ac484008c0
Narydiniz <senainaryara@gmail.com>
2024-11-26 10:49:33 -0300

    Trabalhos mais procurados

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Troca de imagens das categorias
+// Troca de imagens das categorias ao passar o mouse
 document.addEventListener('DOMContentLoaded', () => {
     const servico = document.querySelectorAll('.servicos');
   
-    servico.forEach(item => {  // Alterei para 'item'
+    servico.forEach(item => { 
         const imagem = item.querySelector('.img-categorias');
         const imagemOriginal = item.getAttribute('data-imagem-original');
         const imagemTroca = item.getAttribute('data-imagem-troca');
@@ -17,4 +17,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  
+  // 
